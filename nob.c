4fa745f0610f38f21b7a8e54354e7f81836e6182
Ramil Khasanshin <khasanshin.ramil@list.ru>
2025-10-31 00:00:08 +0300

    build(fix): added building separate *.o files for targets

--- nob.c
+++ nob.c
@@ -12,6 +12,7 @@
 #elif __MINGW32__
 #define DEFAULT_TARGET "windows"
 #else
+#define DEFAULT_TARGET "unsupported"
 #error "Unsupported platform."
 #endif
 
@@ -43,45 +44,58 @@ int main(int argc, char** argv) {
 		exit(0);
 	}
 
+	// Checking target
+	int target = -1; // TODO: enum + separate func
+	if (strcmp(*f_target, "linux") == 0)   target = 0;
+	if (strcmp(*f_target, "windows") == 0) target = 1;
+	if (target == -1) {
+		nob_log(ERROR, "No such target.");
+		exit(1);
+	}
+
 	// Building
 
 	Cmd cmd = {0};
 
 	if (!nob_mkdir_if_not_exists("./build")) return 1;
 
-
-	if (nob_needs_rebuild1("build/mongoose.o", MONGOOSE"/mongoose.c")) {
-		nob_cmd_append(&cmd, *f_cc, MONGOOSE"/mongoose.c", "-c", "-o", "build/mongoose.o");
+	if (nob_needs_rebuild1(nob_temp_sprintf("build/mongoose_%s.o", *f_target), MONGOOSE"/mongoose.c")) {
+		nob_cmd_append(&cmd, *f_cc, MONGOOSE"/mongoose.c", "-c", "-o");
+		nob_cmd_append(&cmd, nob_temp_sprintf("build/mongoose_%s.o", *f_target));
 		if (!cmd_run(&cmd)) return 1;
 	}
+	nob_temp_reset();
 
-	if (strcmp(*f_target, "linux") == 0) {
-		nob_cmd_append(&cmd, *f_cc, "main.c");
-		nob_cmd_append(&cmd, "build/mongoose.o");
-		nob_cmd_append(&cmd, "-I"MONGOOSE);
-		//nob_cmd_append(&cmd, LINUX_RAYLIB"/lib/libraylib.a");
-		//nob_cmd_append(&cmd, "-I"LINUX_RAYLIB"/include");
-		nob_cmd_append(&cmd, "-o", LINUX_OUTPUT);
-		nob_cmd_append(&cmd, "-lm");
-		if (!cmd_run(&cmd)) return 1;
-		if (*f_run) {
-			nob_cmd_append(&cmd, LINUX_OUTPUT);
+	switch (target) {
+		case 0:
+			nob_cmd_append(&cmd, *f_cc, "main.c");
+			nob_cmd_append(&cmd, "build/mongoose_linux.o");
+			nob_cmd_append(&cmd, "-I"MONGOOSE);
+			//nob_cmd_append(&cmd, LINUX_RAYLIB"/lib/libraylib.a");
+			//nob_cmd_append(&cmd, "-I"LINUX_RAYLIB"/include");
+			nob_cmd_append(&cmd, "-o", LINUX_OUTPUT);
+			nob_cmd_append(&cmd, "-lm");
 			if (!cmd_run(&cmd)) return 1;
-		}
-	} else if (strcmp(*f_target, "windows") == 0) {
-		nob_cmd_append(&cmd, *f_cc, "main.c");
-		nob_cmd_append(&cmd, "build/mongoose.o");
-		nob_cmd_append(&cmd, "-I"MONGOOSE);
-		//nob_cmd_append(&cmd, WINDOWS_RAYLIB"/lib/libraylib.a");
-		//nob_cmd_append(&cmd, "-I"WINDOWS_RAYLIB"/include");
-		nob_cmd_append(&cmd, "-o", WINDOWS_OUTPUT);
-		nob_cmd_append(&cmd, "-mwindows");
-		nob_cmd_append(&cmd, "-lws2_32");
-		if (!cmd_run(&cmd)) return 1;
-		if (*f_run) {
-			nob_cmd_append(&cmd, WINDOWS_OUTPUT);
+			if (*f_run) {
+				nob_cmd_append(&cmd, LINUX_OUTPUT);
+				if (!cmd_run(&cmd)) return 1;
+			}
+			break;
+		case 1:
+			nob_cmd_append(&cmd, *f_cc, "main.c");
+			nob_cmd_append(&cmd, "build/mongoose_windows.o");
+			nob_cmd_append(&cmd, "-I"MONGOOSE);
+			//nob_cmd_append(&cmd, WINDOWS_RAYLIB"/lib/libraylib.a");
+			//nob_cmd_append(&cmd, "-I"WINDOWS_RAYLIB"/include");
+			nob_cmd_append(&cmd, "-o", WINDOWS_OUTPUT);
+			nob_cmd_append(&cmd, "-mwindows");
+			nob_cmd_append(&cmd, "-lws2_32");
 			if (!cmd_run(&cmd)) return 1;
-		}
+			if (*f_run) {
+				nob_cmd_append(&cmd, WINDOWS_OUTPUT);
+				if (!cmd_run(&cmd)) return 1;
+			}
+			break;
 	}
 
 	return 0;
